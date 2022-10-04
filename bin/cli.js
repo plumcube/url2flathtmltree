245a910a7585d86f97a2e1a7914a5f8487d4cf6a
Ben Ng <cngaa@amazon.com>
2022-10-04 11:05:23 -0400

    init

---
+++ bin/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+'use strict'
+
+const { JSDOM } = require('jsdom');
+const url = process.argv[2] || "https://www.amazon.com";
+
+const traverseDOMBFS = window => {
+
+    const { document } = window;
+    const queue = [];
+    // const colorSequence = [];
+    queue.push(document.querySelector('body'));
+
+    while (queue.length) {
+        const node = queue.shift();
+        try {
+            const color = window.getComputedStyle(node).color;
+            process.stdout.write(rgba2hex(color) + ', ')
+            // colorSequence.push(color);
+        } catch (e) {}
+
+        for (let i = 0; i < node.children.length; i++) {
+            queue.push(node.children[i]);
+        }
+    }
+};
+
+const rgba2hex = (rgba) => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`
+
+
+
+JSDOM.fromURL(url).then((dom) => {
+    traverseDOMBFS(dom.window);
+});
